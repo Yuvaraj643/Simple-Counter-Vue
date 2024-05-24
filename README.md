afbe57e2e302ded5a6921fbac7450ce6762631d2
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2024-05-24 12:12:58 +0530

    added store

---
+++ README.md
@@ -0,0 +1,35 @@
+# Counter App
+
+This is a simple counter app built with Vue 3 and Pinia.
+
+## Getting Started
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/your-username/counter-app.git
+```
+
+2. Install dependencies
+
+```bash
+cd counter-app
+npm install
+```
+
+3. Start the Development Server
+
+```bash
+npm run dev
+```
+
+## Features
+
+1. Increment and decrement the counter
+2. Reset the counter to 0
+
+
+## Built With
+- Vue 3
+- Pinia
+
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="main-section">
+    <header>
+      <h1>Simple Counter App</h1>
+    </header>
+
+    <p class="count">{{ counterStore.count }}</p>
+    <div class="counter">
+      <button @click="counterStore.decrement">Decrement</button>
+
+      <button @click="counterStore.reset">Reset</button>
+      <button @click="counterStore.increment">Increment</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useCounterStore } from "@/stores/counter";
+export default {
+  setup() {
+    const counterStore = useCounterStore();
+
+    return {
+      counterStore,
+    };
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.main-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+
+.count {
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.counter {
+  display: flex;
+  gap: 30px;
+}
+
+.counter button {
+  font-size: 15px;
+  padding: 15px 20px;
+  border-radius: 15px;
+}
+</style>
